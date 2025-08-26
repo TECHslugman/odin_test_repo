724f5383ade18a36b01ad3ac488bb72c274c97de
TECHslugman <pradeeppokhrl2022@gmail.com>
2025-08-26 12:36:57 +0600

    First part of the js files are added

---
+++ Data_Types_Conditionals.js
@@ -0,0 +1,28 @@
+let name = "Pradeep";
+console.log(`Hello ${name}`);
+
+let islesser = 4>1;
+console.log(islesser);
+
+let string = "This is a string";
+console.log(string);
+let badstring = string;
+console.log(badstring);
+
+let embeded = "And this is Pradeep";
+console.log(`hello there, this is to see how well embedded script works ${embeded}`);
+
+const name1 = "Pradeep";
+const greetings = `Hello ${name1}`;
+console.log(greetings);
+
+const marks = 86;
+const total = 100;
+const output = `My name is ${name1} and my CGPA is ${
+    (marks/total)*5
+} `
+console.log(output);
+
+let StringLength = "Applebananamangoorange";
+let CountLength = StringLength.length;
+console.log(CountLength);
---
+++ Numbers_Variables.js
@@ -0,0 +1,53 @@
+
+console.log(23 + 97);
+console.log((4+6+9)/77);
+
+let a = 10;
+console.log(a);
+ a = 12;
+console.log(a);
+
+let b = 7 * a;
+console.log(b);
+
+const max = 57;
+const actual = max - 13;
+const percent = actual/max;
+console.log (percent);
+const myInt = 5;
+const myFloat = 6.667;
+console.log(myInt);
+console.log(myFloat);
+
+typeof myInt;
+typeof myFloat;
+const LostOfDecimal = 7.812828373232;
+console.log(LostOfDecimal);
+const TwoDecimalPoint = LostOfDecimal.toFixed(2);
+console.log(TwoDecimalPoint);
+
+let mynum = "77";
+console.log(mynum);
+console.log(typeof mynum);
+console.log(mynum = Number(mynum) + 3);
+
+let num1 = 4;
+num1++;
+console.log(num1);
+console.log(num1);
+num1--;
+console.log(num1);
+
+let x = 2;
+x = -x;
+console.log(x);
+
+let y = "Pradeep " + "Pokhrel";
+console.log(y);
+
+let apples = "2";
+let bananas = "3";
+console.log( apples + bananas);
+console.log(Number (apples) + Number(bananas));
+console.log( +apples + +bananas);
+
